b6eeedcce67e0ec3dd94e44aaffc2d22b1616f84
Ted Vu <ted.vu@sourcecode.com.au>
2022-11-11 00:32:18 +0700

    Update lspsaga nvim

--- nvim/after/plugin/lspsaga.rc.lua
+++ nvim/after/plugin/lspsaga.rc.lua
@@ -1,7 +1,21 @@
 local keymap = vim.keymap.set
 local saga = require('lspsaga')
 
-saga.init_lsp_saga()
+saga.init_lsp_saga({
+  code_action_icon = "",
+  code_action_lightbulb = {
+    enable = false,
+    enable_in_insert = false,
+    cache_code_action = false,
+    sign = false,
+    update_time = 150,
+    sign_priority = 20,
+    virtual_text = false,
+  },
+  diagnostic_header = { " ", " ", " ", "ﴞ "},
+  border_style = "rounded",
+})
+
 
 keymap("n", "gh", "<cmd>Lspsaga lsp_finder<CR>", { silent = true })
 keymap("n", "gp", "<cmd>Lspsaga peek_definition<CR>", { silent = true })
@@ -9,3 +23,4 @@ keymap("n", "gs", "<cmd>Lspsaga signature_help<CR>", { silent = true })
 keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>", { silent = true })
 keymap("n", "J", "<cmd>Lspsaga diagnostic_jump_next<CR>", { silent = true })
 keymap("n", ",,", "<cmd>Lspsaga code_action<CR>", { silent = true })
+
